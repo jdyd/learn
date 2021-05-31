2798755d86270342669ebd342e4e4a8904255704
cuijian03 <cuijian03@meituan.com>
2021-05-31 10:21:52 +0800

    es-learn

---
+++ fsm-learn/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author: cuijian03
+ * @description:
+ * @create: 2021-05-29 14:45
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int aa = 2;
+        System.out.println(a % aa);
+
+    }
+}
